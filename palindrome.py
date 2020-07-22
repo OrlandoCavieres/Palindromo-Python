dec05415f79717f3f900c8bcfcf8d0303c19c9dd
Orlando Cavieres <frizortake@gmail.com>
2020-07-22 16:41:24 -0400

    Correcion de metodo inexistente en funcion esPalindromo. Completado cuerpo funcion main

--- palindrome.py
+++ palindrome.py
@@ -7,13 +7,32 @@ def esPalindromo(frase):
         Boolean: Es o no es un palindromo.
     """
     frase = frase.replace(" ","")
-    if frase == frase.reverse():
+    if frase == frase[::-1]:
         return True
     else:
         return False
 
 def main():
-    pass
+    salir = False
+    while not salir:
+        fraseIngresada = input("\nIngrese una frase, oración o palabra: ").strip()
+        largoTexto = len(fraseIngresada) + 25
+        print("\n" + "*" * largoTexto)
+        if esPalindromo(fraseIngresada):
+            print(f"'{fraseIngresada}' SI es un palindromo")
+        else:
+            print(f"'{fraseIngresada}' NO es un palindromo")
+        print("*" * largoTexto)
+        while True:
+            desicionSalir = input("\nIngrese una opcion:\n\n1 - Salir\n2 - Continuar e ingresar otra frase\n\nDesea seguir? ")
+            if desicionSalir == "1":
+                print("\nHasta luego!\n")
+                salir = True
+                break
+            elif desicionSalir == "2":
+                break
+            else:
+                print("\nIngrese una opción válida")
 
 if __name__ == '__main__':
     main()
